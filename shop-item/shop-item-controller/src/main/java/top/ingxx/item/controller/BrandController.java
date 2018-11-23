da65376aa2b95bae6ceb64a6702b8fa77cdfb4c0
e <q523969705@gmail.com>
2018-11-23 23:25:42 +0800

    跨域问题

--- shop-item/shop-item-controller/src/main/java/top/ingxx/item/controller/BrandController.java
+++ shop-item/shop-item-controller/src/main/java/top/ingxx/item/controller/BrandController.java
@@ -2,6 +2,7 @@ package top.ingxx.item.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import top.ingxx.item.service.BrandService;
 
 import java.util.List;
 
+/// TODO: 2018/11/23  这里使用CORS暂时解决跨域问题 明天使用zuul和nginx解决
+@CrossOrigin
 @RestController
 @RequestMapping("brand")
 public class BrandController {
@@ -17,6 +20,7 @@ public class BrandController {
     @Autowired
     private BrandService brandService;
 
+    @CrossOrigin
     @GetMapping("/findAll")
     public ResponseEntity<List<Brand>> findAll() {
         return ResponseEntity.ok(brandService.selectAll());
