c5d4304265e709fb9e61e36b88579b3267451ac2
e <q523969705@gmail.com>
2018-11-24 09:59:32 +0800

    配置zuul网关，修改端口号，使用EnableDiscoveryClient注解

--- shop-item/shop-item-controller/src/main/java/top/ingxx/ItemApplication.java
+++ shop-item/shop-item-controller/src/main/java/top/ingxx/ItemApplication.java
@@ -2,11 +2,12 @@ package top.ingxx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @MapperScan("top.ingxx.item.dao")
 public class ItemApplication {
     public static void main(String[] args) {
---
+++ shop-zuul-server/src/main/java/top/ingxx/zuul/zuulApplication.java
@@ -0,0 +1,13 @@
+package top.ingxx.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class zuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(zuulApplication.class,args);
+    }
+}
